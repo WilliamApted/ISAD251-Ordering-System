f75f0bafe3d4335a1113c889f080b30cd7a1547c
William Apted <willapted@outlook.com>
2019-11-20 14:16:36 +0000

    Addition of database context class
    Beginning linking to database.

---
+++ OrderingSystem/OrderingSystem/Models/Database/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderingSystem.Models.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Admin> Administrator { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<BasketItem> BasketItem { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Category> Categpry { get; set;}
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
