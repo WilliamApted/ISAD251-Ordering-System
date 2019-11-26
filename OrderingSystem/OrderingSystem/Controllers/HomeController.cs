65d63aa5e55452402e2c179e01a473dc9585614d
William Apted <william.apted@students.plymouth.ac.uk>
2019-11-26 17:23:16 +0000

    Removal of customer registration
    Removing custom registration and login. Moving to just confirming order with first name. Order can then be viewed with order Id and first name.

--- OrderingSystem/OrderingSystem/Controllers/HomeController.cs
+++ OrderingSystem/OrderingSystem/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OrderingSystem.Models.Database;
-using OrderingSystem.Models.UserAccount;
 
 namespace OrderingSystem.Controllers
 {
--- OrderingSystem/OrderingSystem/Models/AdminAccount/LoginModel.cs
+++ OrderingSystem/OrderingSystem/Models/AdminAccount/LoginModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OrderingSystem.Models.UserAccount
+namespace OrderingSystem.Models.AdminAccount
 {
     public class LoginModel
     {
