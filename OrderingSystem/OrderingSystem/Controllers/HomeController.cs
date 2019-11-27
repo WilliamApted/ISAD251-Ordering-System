740ae8bb32e4935c95b236baeb8dda7ae1b55252
William Apted <willapted@outlook.com>
2019-11-27 15:07:01 +0000

    Finalising cookie basket function.
    Creating the basket functionality. Still requires addition of stored procedures.

--- OrderingSystem/OrderingSystem/Controllers/HomeController.cs
+++ OrderingSystem/OrderingSystem/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OrderingSystem.Models.Database;
+using OrderingSystem.Models.Items;
 
 namespace OrderingSystem.Controllers
 {
@@ -32,52 +34,90 @@ namespace OrderingSystem.Controllers
         }
 
         
-        public IActionResult UpdateOrder(int itemId)
+        public IActionResult UpdateOrderAsync(int itemId)
         {
-            //Response.Cookies.Delete("Basket");
+            string cookieValue = Request.Cookies["Basket"];
+            List<CookieBasketModel> basket;
 
-            /*
-            if (Request.Cookies["Basket"] != null)
+            //Check if list exists in cookie, if it does, get it, then add or remove from list.
+            if (cookieValue != null && cookieValue != "")
             {
-                Set("Basket", itemId.ToString());
+                //Cookie exists, so get the data 
+                basket = JsonSerializer.Deserialize<List<CookieBasketModel>>(cookieValue);
+                basket = AddItemToBasket(basket, itemId);
             }
             else 
             {
-                Set("Basket", "1");
+                //Doesnt exist, so create new list
+                basket = new List<CookieBasketModel>();
+                basket = AddItemToBasket(basket, itemId);
             }
-            */
-            Set("Basket", itemId.ToString());
+            cookieValue = JsonSerializer.Serialize(basket);
 
+            //Response.Cookies.Delete("Basket");
 
-            return PartialView("/Views/Shared/Menu/_Basket.cshtml", itemId.ToString());
+            Set("Basket", cookieValue);
+            List<BasketItemModel> basketModel = GetBasketItemList(basket);
+            return PartialView("/Views/Shared/Menu/_Basket.cshtml", basketModel);
         }
 
+        /// <summary>
+        /// Gets the basketItem Models to display the current order basket.
+        /// </summary>
+        /// <param name="cookieBasket">List of items in the basket.</param>
+        /// <returns>List of BasketItem Models.</returns>
+        public List<BasketItemModel> GetBasketItemList(List<CookieBasketModel> cookieBasket) 
+        {
+            List<BasketItemModel> basketItems = new List<BasketItemModel>();
 
+            foreach (CookieBasketModel item in cookieBasket) 
+            {
+                //Perhaps use stored procedure to get this...
+                var menuQuery = from tempitem in _context.Item where tempitem.Id == item.ItemId select tempitem;
+                Item items = menuQuery.First();
+                //Adds the Basket Item model to the list
+                basketItems.Add(new BasketItemModel() { Name = items.Name, Quantity = item.Quantity, Price = items.Price, ImgUrl = items.ImageUrl });
+            }
 
+            return basketItems;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        /// <summary>
+        /// Adds an item to the basket list.
+        /// </summary>
+        /// <param name="basket">Current basket list.</param>
+        /// <param name="itemId">Item to add.</param>
+        /// <returns>Updated basket list.</returns>
+        public List<CookieBasketModel> AddItemToBasket(List<CookieBasketModel> basket, int itemId) 
+        {
+            //Check if item exists, if it does, add quantity.
+            foreach (CookieBasketModel item in basket)
+            {
+                if (item.ItemId == itemId)
+                {
+                    item.Quantity++;
+                    return basket;
+                }
+            }
+            //If it doesnt exist, add it here.
+            basket.Add(new CookieBasketModel() { ItemId = itemId, Quantity = 1 });
+            return basket;
+        }
 
         /// <summary>  
         /// set the cookie  
         /// </summary>  
-        /// <param name="key">key (unique indentifier)</param>  
-        /// <param name="value">value to store in cookie object</param>  
+        /// <param name="key">Cookie identifier.</param>  
+        /// <param name="value">String to store in the cookie.</param>  
         public void Set(string key, string value)
         {
             CookieOptions option = new CookieOptions();
             option.Expires = DateTime.Now.AddDays(7);
             Response.Cookies.Append(key, value, option);
         }
+
+
+
+
     }
 }
---
+++ OrderingSystem/OrderingSystem/Models/Items/CookieBasketModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderingSystem.Models.Items
+{
+    public class CookieBasketModel
+    {
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
